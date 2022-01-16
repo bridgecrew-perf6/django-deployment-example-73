3c52cfb927007a246efd74949eaf6c3f8fecdba7
CeemaRajith <ceemarajith@in.ibm.com>
2022-01-16 16:03:45 +0530

    updated code

--- learning_users/basic_app/admin.py
+++ learning_users/basic_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from basic_app.models import UserProfileInfo, User
+from basic_App.models import UserProfileInfo
 
 # Register your models here.
 admin.site.register(UserProfileInfo)
